f69f99b1791c4c351872b4a83eff15e8dc939724
Jenan Mannette <jenanmannette@gmail.com>
2015-05-10 18:23:12 -0600

    Homework - these work, but not sure if the best way to solve, also struggled with the fibo sequence

---
+++ fiboNum.js
@@ -0,0 +1,8 @@
+var fib = function(num){
+  for(i=0,j=1,k=0; k<num;i=j,j=x,k++ ){
+   x=i+j;
+   console.log(x);
+  }
+}
+
+fib(10);
---
+++ numSeq.js
@@ -0,0 +1,10 @@
+var sequence = function(num) {
+  var number = 1;
+  while (number <= 64) {
+      console.log(number);
+        number = number * 2;
+  };
+  return number;
+};
+
+sequence(1);
